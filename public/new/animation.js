8393102cb29a8e20e8bdcb5c52f9fd3462a1a3b5
Thr1ve <fict1on.buhler@gmail.com>
2015-11-07 15:52:16 -0500

    add separate animation function

---
+++ public/new/animation.js
@@ -0,0 +1,17 @@
+var setFrame = function(animation, frame) {
+  var element = {};
+  // if we just got a single image, we don't need to pick a frame...
+  if (frame === undefined) {
+    element.backgroundImage = animation[0];
+    element.backgroundPosition = animation[1];
+    element.width = animation[2];
+    element.height = animation[3];
+  // otherwise, pick the frame
+  } else {
+    element.backgroundImage = animation[frame][0];
+    element.backgroundPosition = animation[frame][1];
+    element.width = animation[frame][2];
+    element.height = animation[frame][3];
+  }
+  return element;
+};
