fa9ec70d644075f65639f3e3e178b7be0495c8db
Thr1ve <fict1on.buhler@gmail.com>
2015-11-07 15:52:02 -0500

    add separate render function

---
+++ public/new/render.js
@@ -0,0 +1,14 @@
+var render = function(domElement, element) {
+  domElement.style.top = element.y + 'px';
+  domElement.style.left = element.x + 'px';
+  domElement.style.height = element.height + 'px';
+  domElement.style.width = element.width + 'px';
+  if (element.backgroundImage) {
+    domElement.style.backgroundImage = element.backgroundImage;
+    domElement.style.backgroundPosition = element.backgroundPosition;
+  } else if (element.backgroundColor) {
+    domElement.style.backgroundColor = element.backgroundColor;
+  }
+};
+
+module.exports = render;
