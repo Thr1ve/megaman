e75ea417d5cf980e6f250bae0628e56211bc87c4
Thr1ve <fict1on.buhler@gmail.com>
2015-11-14 12:15:30 -0500

    move object functions to objects folder

---
+++ public/new/library/utilities/objects/objects.js
@@ -0,0 +1,10 @@
+/* global clone, compareKeys, deepFreeze, mapValues, merge, mergeNew */
+
+var objects = {
+  clone: clone,
+  compareKeys: compareKeys,
+  deepFreeze: deepFreeze,
+  mapValues: mapValues,
+  merge: merge,
+  mergeNew: mergeNew,
+};
